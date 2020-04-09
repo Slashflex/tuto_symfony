bd0ee3eb4792ea301dda4e2e6144e782a110d0a3
David <pro.davidsaoud@gmail.com>
2020-04-09 12:27:57 +0200

    display parameters

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,19 +11,28 @@ class HomeController extends AbstractController
     /**
      * @Route("/home", name="home")
      */
-    public function index() :Response
+    public function index(): Response
     {
         $user = [
-            'user1' => 'david',
-            'user2' => 'arnaud',
-            'user3' => 'quentin'
+            'user1' => [
+                'name' => 'David',
+                'age' => 35
+            ],
+            'user2' => [
+                'name' => 'Arnaud',
+                'age' => 25
+            ], 
+            'user3' => [
+                'name' => 'Quentin',
+                'age' => 25
+            ],
         ];
 
         $test = 'un message de test';
 
         return $this->render('home/home.html.twig', [
             'user' => $user,
-            'test' => $test
+            'test' => ucfirst($test)
         ]);
     }
 }
